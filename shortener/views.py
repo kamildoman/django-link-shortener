c7b7c01238ccc799012f6c1f89b2fca2a55f441f
kamildoman <domanweb1@gmail.com>
2021-11-02 21:19:54 +0100

    checkurl

--- shortener/views.py
+++ shortener/views.py
@@ -3,11 +3,11 @@ from .models import URLShortener
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import URLForm
-import validators
 
 
 def index(request):
     if request.method == "POST":
+        error = ""
         form = URLForm(request.POST)
         form.save(commit=False)
         link = form.cleaned_data["url"]
@@ -18,7 +18,10 @@ def index(request):
             form.save()         
             short_link = URLShortener.objects.get(url=link).shortened_url
         form = URLForm
-        return render(request, "shortener/index.html", {"form": form, "short_link": short_link})
+        if "www" not in link and "http" not in link:
+            short_link = ""
+            error = "Please enter a valid url"
+        return render(request, "shortener/index.html", {"form": form, "short_link": short_link, "error": error})
     else:
         form = URLForm
         return render(request, "shortener/index.html", {"form": form})
@@ -27,8 +30,7 @@ def redirect_url(request, entered_url):
     try:
         link = URLShortener.objects.get(shortened_url = entered_url).url
         if "http" not in link:
-            link = f"https://{link}"
-        print(link)
+            link = f"http://{link}"
         return HttpResponseRedirect(link)
     except:
         return HttpResponse(entered_url)
